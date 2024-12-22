cd5f717bdc3c9c0a6ee9462e23256e79e5453d09
Business Pro Legal <Pro@businesegalsair.home>
2024-12-22 15:50:38 +0400

    Initial commit

---
+++ test_import.py
@@ -0,0 +1,4 @@
+# test_import.py
+from utils.helpers import get_table_csv_results
+
+print(get_table_csv_results("test.pdf"))
---
+++ utils/helpers.py
@@ -0,0 +1,3 @@
+def get_table_csv_results(pdf_file):
+    """Processes a PDF file and extracts table data as CSV."""
+    return "column1,column2\nvalue1,value2"
